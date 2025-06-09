e3afde010caf53c61bf34e7904f4d250e118ccc2
Fioru Org Administrator <caoimhinaodh@gmail.com>
2025-06-09 14:30:42 +0100

    Let there be CI (#2)

--- wsh/lib/functions.d/11-string.sh
+++ wsh/lib/functions.d/11-string.sh
@@ -14,3 +14,38 @@ function _string_chomp {
     local s="$(echo "${@}" | sed -e 's/^ *//g;s/ *$//g')"
     echo "${s}"
 }
+
+
+# Reproduces the behaviour of trim() to remove leading and trail spaces
+# Reproduced from the awesome: https://github.com/dylanaraps/pure-bash-bible
+function _string_trim() {
+    # Usage: trim_string "   example   string    "
+    : "${1#"${1%%[![:space:]]*}"}"
+    : "${_%"${_##*[![:space:]]}"}"
+    printf '%s\n' "$_"
+}
+
+
+# Reproduces the behaviour of split()
+# Reproduced from the awesome: https://github.com/dylanaraps/pure-bash-bible
+function _string_split() {
+   # Usage: split "string" "delimiter"
+   IFS=$'\n' read -d "" -ra arr <<< "${1//$2/$'\n'}"
+   printf '%s\n' "${arr[@]}"
+}
+
+
+# Reproduces the behaviour of lower()
+# Reproduced from the awesome: https://github.com/dylanaraps/pure-bash-bible
+function _string_lower() {
+    # Usage: lower "string"
+    printf '%s\n' "${1,,}"
+}
+
+
+# Reproduces the behaviour of upper()
+# Reproduced from the awesome: https://github.com/dylanaraps/pure-bash-bible
+function _string_upper() {
+    # Usage: upper "string"
+    printf '%s\n' "${1^^}"
+}
